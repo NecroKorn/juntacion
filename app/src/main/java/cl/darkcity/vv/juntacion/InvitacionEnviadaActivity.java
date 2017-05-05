f05cf1d950f60d62f953ab586a4e45f2126c2538
Marcelo Vega Janhsen <necrokorn@gmail.com>
2017-05-05 14:03:55 -0300

    Se crean la activities base

---
+++ app/src/main/java/cl/darkcity/vv/juntacion/InvitacionEnviadaActivity.java
@@ -0,0 +1,13 @@
+package cl.darkcity.vv.juntacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvitacionEnviadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitacion_enviada);
+    }
+}
